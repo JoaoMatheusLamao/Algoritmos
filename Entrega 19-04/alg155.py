4b40464b88c7efebc2fcd38920870baa98877c4c
João Matheus Lamão <joaomatheuslamao9@gmail.com>
2023-04-13 00:10:48 -0300

    alg155

--- Entrega 19-04/alg155.py
+++ Entrega 19-04/alg155.py
@@ -1,7 +1,7 @@
 pal = input('Entre com uma palavra: ')
 letra1 = pal[0].lower()
 if letra1 == 'l' or letra1 == 'd':
-    nv_pal = pal[0:1], pal[-1]
+    nv_pal = pal[0:2] + pal[-1]
 else:
     nv_pal = pal[1:]
 print(f'Nova palavra: {nv_pal}')
