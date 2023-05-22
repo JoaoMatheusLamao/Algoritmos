9d0bdc6bd95d9e303048ade7f5e50ad7b0665b93
João Matheus Lamão <joaomatheuslamao9@gmail.com>
2023-05-22 16:42:25 -0300

    bora la

---
+++ Semana 22-05/exercicios_lista.py
@@ -0,0 +1,42 @@
+# 1. Leia um vetor de 10 elementos e calcule o maior e o menor valor.
+
+
+# 2. Agora usando listas, indique como um troco deve ser dado utilizando-se um número mínimo de
+# notas. Seu algoritmo deve ler o valor da conta a ser paga e o valor do pagamento efetuado
+# desprezando os centavos. Suponha que as notas para troco sejam as de 50, 20, 10, 5, 2 e 1
+# reais, e que nenhuma delas esteja em falta no caixa.
+
+
+# 3. Faça um Programa que leia 20 números inteiros e armazene-os num vetor. Armazene os
+# números pares no vetor PAR e os números IMPARES no vetor impar. Imprima os três vetores.
+
+
+# 4. Faça um Programa que peça as quatro notas de 10 alunos, calcule e armazene num vetor a
+# média de cada aluno, imprima o número de alunos com média maior ou igual a 7.0.
+
+
+# 5. Faça um Programa que leia um vetor de 5 números inteiros, mostre a soma, a multiplicação e
+# os números.
+
+
+# 6. Faça um Programa que peça a idade e a altura de 5 pessoas, armazene cada informação no
+# seu respectivo vetor. Imprima a idade e a altura na ordem inversa a ordem lida.
+
+
+# 7. Faça um Programa que leia um vetor A com 10 números inteiros, calcule e mostre a soma dos
+# quadrados dos elementos do vetor.
+
+
+# 8. Faça um Programa que leia dois vetores com 10 elementos cada. Gere um terceiro vetor de 20
+# elementos, cujos valores deverão ser compostos pelos elementos intercalados dos dois outros
+# vetores.
+
+
+# 9. Foram anotadas as idades e alturas de 30 alunos. Faça um Programa que determine quantos
+# alunos com mais de 13 anos possuem altura inferior à média de altura desses alunos.
+
+
+# 10.Faça um programa que receba a temperatura média de cada mês do ano e armazene-as em
+# uma lista. Após isto, calcule a média anual das temperaturas e mostre todas as temperaturas
+# acima da média anual, e em que mês elas ocorreram (mostrar o mês por extenso: 1 – Janeiro,
+# 2 – Fevereiro, . . . ).
